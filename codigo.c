78a98b3032c7d6e33eb0149af351c0dd45dd6f92
Gabriel <gabrielivo37@gmail.com>
2021-08-15 22:33:18 -0300

    Add files via upload

---
+++ codigo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// Author: Gabriel Ivo Lins
+// aprendendo a usar estrutura switch.
+
+int main(){
+
+    int dia;
+
+    printf("Digite de 1 a 7:\n");
+    scanf("%d", &dia);
+
+    switch (dia)
+    {
+    case 1:
+        printf("Segunda-feira\n");
+        break;
+
+    case 2:
+        printf("Terça-feira\n");    
+        break;
+    
+    case 3:
+        printf("Quarta-feira\n");
+
+    case 4:
+        printf("Quinta-feira\n");
+        break;
+    
+    case 5:
+        printf("Sexta-feira\n");
+        break;
+    
+    case 6:
+        printf("Sabado\n");
+        break;
+    
+    case 7:
+        printf("Domingo\n");
+        break;
+    
+    default:
+        printf("Você não digitou um valor valido!\n");
+        break;
+    }
+
+    return 0;
+}
